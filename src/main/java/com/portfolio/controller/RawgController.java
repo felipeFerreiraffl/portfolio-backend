328f4d768ca1884fe7a025a7320ea3e0d2c5e60b
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-15 15:11:40 -0300

    feat: new RAWG proxy connection

--- src/main/java/com/portfolio/controller/RawgController.java
+++ src/main/java/com/portfolio/controller/RawgController.java
@@ -1,4 +1,29 @@
 package com.portfolio.controller;
 
+import com.portfolio.service.RawgService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/rawg")
 public class RawgController {
+    private final RawgService serv;
+
+    // Injeta a dependência
+    public RawgController(RawgService serv) {
+        this.serv = serv;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getGameById(@PathVariable int id) {
+        String response = serv.getGameById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<String> getGamesByFilter(@RequestParam String ordering, @RequestParam int pageSize) {
+        String response = serv.getGamesByFilter(ordering, pageSize);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/com/portfolio/service/RawgService.java
+++ src/main/java/com/portfolio/service/RawgService.java
@@ -1,4 +1,46 @@
 package com.portfolio.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@Service
 public class RawgService {
+
+    // Permite a conexão com outras APIs
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Value("${api.rawg.url}")
+    private String rawgApiUrl;
+    @Value("${api.rawg.key}")
+    private String rawgApiKey;
+
+    // Busca informações de um jogo específico
+    public String getGameById(int id) {
+        // Cria uma URI
+        URI uri = UriComponentsBuilder
+                .fromUriString(rawgApiUrl + "/" + id)
+                .queryParam("key", rawgApiKey)
+                .build()
+                .toUri();
+
+        // Converte a URI em String
+        return restTemplate.getForObject(uri, String.class);
+    }
+
+    // Busca jogos por filtro (popularidade, por nota, etc)
+    public String getGamesByFilter(String ordering, int pageSize) {
+        URI uri = UriComponentsBuilder
+                .fromUriString(rawgApiUrl)
+                .queryParamIfPresent("ordering", Optional.ofNullable(ordering)) // Evita valores nulos
+                .queryParam("page_size", pageSize)
+                .queryParam("key", rawgApiKey)
+                .build()
+                .toUri();
+
+        return restTemplate.getForObject(uri, String.class);
+    }
 }
