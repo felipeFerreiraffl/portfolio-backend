81b33421a6bba9adbd59a90b2e48f98e74c97732
felipeFerreiraffl <felipado2x0@gmail.com>
2025-05-16 19:23:23 -0300

    feat: added CORS configuration for frontend

---
+++ src/main/java/com/portfolio/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.portfolio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry reg) {
+        reg.addMapping("/**")
+                .allowedHeaders("*") // Adicionar o localhost e o site da Vercel posteriormente
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
+    }
+}
